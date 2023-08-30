d6ba4b7eb88b649bd3377d75686c81992b83cde9
areebps <areeb@parallelsolutuions.com.pkgit config --global user.name areeb-parallel-solutions>
2023-08-30 12:34:09 +0500

    React redux toolkit

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+// This will store the whole state of the app in an immutable object tree
+// immutable means cannot be changed
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {
+        
+    }
+}); 
